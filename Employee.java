d491b0391d327e3ae184708771e31be1d486ad6d
venisubha <subhapcse@gmail.com>
2018-10-27 07:34:35 +0530

    project

---
+++ Employee.java
@@ -0,0 +1,51 @@
+package org.emp;
+
+public class Employee {
+
+	public void empId() {
+		
+		
+	}
+	
+	public void empName() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void empDob() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void empPhone() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void empEmail() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	public void empAddress() {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+	public static void main(String[] args) {
+		
+		Employee objEmployee = new Employee();
+		objEmployee.empId();
+		objEmployee.empName();
+		objEmployee.empPhone();
+	    objEmployee.empDob();
+	    objEmployee.empAddress();
+	    objEmployee.empEmail();
+	
+	}
+	
+	
+
+}
